31e9d7febd2e7d1dfbc46e89a5161949d0b9eafd
ytallohgadelha <ytallolg@gmail.com>
2018-07-12 14:36:49 -0300

    Criação e implementação de uma função para verificar a conexão do dispositivo com a internet

--- app-android-OS/app/src/main/java/com/example/ytallogadelha/app_android_os/MainActivity.kt
+++ app-android-OS/app/src/main/java/com/example/ytallogadelha/app_android_os/MainActivity.kt
@@ -1,12 +1,44 @@
 package com.example.ytallogadelha.app_android_os
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.net.ConnectivityManager
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var textView: TextView = findViewById(R.id.textView)
+
+        verificaConexao(this)
+    }
+
+
+    //Função para verificar a disponibilidade de conexão com a rede
+    private fun verificaConexao(contexto: Context): Boolean {
+
+        val conectado: Boolean
+
+        //Pego a conectividade do contexto o qual o metodo foi chamado
+        val cm = contexto.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+        //Crio o objeto netInfo que recebe as informacoes da Network
+        val netInfo = cm.activeNetworkInfo
+
+        //booleano para saber se há conexão
+        conectado = (netInfo != null) && (netInfo.isConnected) && (netInfo.isAvailable)
+
+        //textView.text = "${conectado}"
+
+        Toast.makeText(this, "${conectado}", Toast.LENGTH_LONG).show()
+
+        return conectado
+
     }
 }
